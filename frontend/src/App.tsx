7edf44c92894aa41e55250e7aaf82dc927f082cf
Markus Kauppila <markus.kauppila@gmail.com>
2017-11-18 18:24:10 +0200

    Add some mock data to the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,16 +5,29 @@ import Title from './components/quiz/title'
 import Question from './components/quiz/questions'
 import Answers from './components/quiz/answers'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Title>Supernatural Quiz</Title>
-        <Question>How many seasons of Supernatural has been produced?</Question>
-        <Answers questions={[]} />
-      </div>
-    );
-  }
+const quiz = {
+  title: 'Supernatural Quiz',
+  question: 'How many seasons of Supernatural has been produced?',
+  answers: [
+    {
+      answer: '8 seasons'
+    },
+    {
+      answer: '14 seasons'
+    },
+    {
+      answer: '12 seasons'
+    },
+    {
+      answer: '6 seasons'
+    },
+  ]
 }
 
-export default App;
+export default () => (
+  <div>
+    <Title>{quiz.title}</Title>
+    <Question>{quiz.question}</Question>
+    <Answers answers={quiz.answers} />
+  </div>
+)
--- frontend/src/components/quiz/answers.tsx
+++ frontend/src/components/quiz/answers.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react'
 
-interface Props {
-  questions: string[]
+interface Answer {
+  answer: string
 }
 
-export default (props: Props) => (
+interface Props {
+  answers: Answer[]
+}
+
+const Answer = ({ answer }: Answer) => (
+  <li><input type="radio" name="answer" value={answer} />{answer}</li>
+)
+
+export default ({ answers }: Props) => (
   <form action="something here">
     <ul>
-      <li><input type="radio" name="answer" value="8s" />8 season</li>
-      <li><input type="radio" name="answer" value="5s" />5 seasons</li>
-      <li><input type="radio" name="answer" value="12s" />12 seasons</li>
-      <li><input type="radio" name="answer" value="14s" />14 seasons</li>
+      {answers.map((answer) => Answer(answer))}
       <input type="submit" name="answer" value="Vastaa" />
     </ul>
   </form>
