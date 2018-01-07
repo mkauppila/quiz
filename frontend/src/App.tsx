7af634bc71d08033dd40da28cf9991646b8f7c84
Markus Kauppila <markus.kauppila@gmail.com>
2018-01-07 18:24:27 +0200

    Add `react-router-dom` and basic routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import styled, { injectGlobal, ThemeProvider } from './styled'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import theme from './theme'
 import quiz from './localMockData'
@@ -7,7 +8,7 @@ import Title from './components/quiz/title'
 import Question from './components/quiz/question'
 import Answers from './components/quiz/answers'
 
-// tslint:disable-line:no-unused-expression
+// tslint:disable-next-line:no-unused-expression
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Vollkorn+SC:600,700');
   @import url('https://fonts.googleapis.com/css?family=Lora');
@@ -30,14 +31,24 @@ const FlexContainer = styled.div`
 const Wrapper = styled.div`
 `
 
-export default () => ( 
-  <ThemeProvider theme={theme}>
-    <FlexContainer>
-      <Wrapper>
-        <Title>{quiz.title}</Title>
-        <Question>{quiz.question}</Question>
-        <Answers answers={quiz.answers} />
-      </Wrapper>
-    </FlexContainer>
-  </ThemeProvider>
+const Content = () => (
+  <FlexContainer>
+    <Wrapper>
+      <Title>{quiz.title}</Title>
+      <Question>{quiz.question}</Question>
+      <Answers answers={quiz.answers} />
+    </Wrapper>
+  </FlexContainer>
+)
+
+export default () => (
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/" component={Content}/>
+        <Route path="/next" component={Content} />
+        <Route render={() => <p>Nothing to see here</p>}/>
+      </Switch>
+    </ThemeProvider>
+  </BrowserRouter>
 )
