7e095be2d103d64c2640643e53b0ae0bd02bbab7
Markus Kauppila <markus.kauppila@gmail.com>
2018-01-28 18:12:53 +0200

    Style frontpage a bit

--- frontend/src/components/quizList/component.tsx
+++ frontend/src/components/quizList/component.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react'
+import styled from '../../styled'
+import { Link } from 'react-router-dom'
+
 import { State } from './index'
+import Title from '../quiz/title'
+
+const LinkListItem = ({ item }: { item: string }) => {
+  const ListItem = styled.li`
+  `
+
+  return (
+    <Link to="/quiz">
+      <ListItem key={item}>{item}</ListItem>
+    </Link>
+  )
+}
+
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
 export default (props: State) => (
-  <div>
-    <p>{props.title}</p>
-    <ul>
-      {props.list.map((item: string) => <li key={item}>{item}</li>)}
-    </ul>
-  </div>
+  <FlexWrapper>
+    <div>
+      <Title>{props.title}</Title>
+      <ul>
+        {props.list.map((item: string) => <LinkListItem key={item} item={item} />)}
+      </ul>
+    </div>
+  </FlexWrapper>
 )
