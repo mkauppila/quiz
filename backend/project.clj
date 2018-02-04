35e0f2807020df5c33c0cb95a55ce1807f3dcb4a
Markus Kauppila <markus.kauppila@gmail.com>
2018-02-04 19:36:44 +0200

    Delete mock up Clojure backend

--- backend/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject quiz "0.1.0-SNAPSHOT"
-  :description "Backend for Quiz app"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [metosin/compojure-api "1.1.10"]]
-  :ring {:handler quiz.handler/app}
-  :uberjar-name "server.jar"
-  :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]]
-                   :plugins [[lein-ring "0.10.0"]]}})
